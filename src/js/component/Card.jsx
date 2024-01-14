2b08feb48902c9c44a1958e421bf95e8d964770b
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-01-14 17:22:46 +0000

    code app

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,20 +2,27 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 export default function Card(){
-    const[todo, setTodo] = useState("Add a new task");
+    const[todo, setTodo] = useState(" ");
     const[count,setCount] = useState(0);
-    const[isDeleted,setDelete] = useState(false);
+    const[isTodelete,setDelete] = useState(false);
 
 //create a function that appends a new div based on input 
 
 function addTask(e){
     const task = e.target.value;
-    console.log(task);
-    return(
-        <p>hey</p>
-    )
-
+    const el = document.getElementById("tasks");
+    if(task != ""){
+        const newDiv= document.createElement("div");
+        newDiv.className="mt-1 mx-4"
+        newDiv.innerHTML=`<i class="fa-solid fa-thumbtack mx-3"></i>`+"  "+task+" "+`<i class="fa-regular fa-trash-can" id="delete"></i>`;
+        el.appendChild(newDiv);
+        const inputField = document.getElementById("input");
+        inputField.value="";
+        setCount(prevCount => prevCount +1);
+    }
 }
+
+
 
 
    
@@ -23,7 +30,9 @@ function addTask(e){
     return(
         <div className="card">
             <h2 className=" d-flex justify-content-center">Tasks</h2>
-            <input className="item" type="text" placeholder="Enter new item" onBlur={(e)=>addTask(e)}/>
+            <input className="item" id="input" type="text" placeholder="Enter new Task" onBlur={(e)=>addTask(e)}/>
+            <div id="tasks"></div>
+            <div className="counter mt-1 mx-4" id="counter">{count}{count==1?" task left!":" tasks left!"}</div>
         </div>
     )
 }
