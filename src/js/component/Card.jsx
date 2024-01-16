ffcef5fc6690ef6985c50470e609c3ceecce2495
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-01-16 20:17:43 +0000

    update scrips

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,37 +2,51 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 export default function Card(){
-    const[todo, setTodo] = useState(" ");
+    const [task,setTask] = useState('')
+    const[list,setList] = useState([]);
     const[count,setCount] = useState(0);
-    const[isTodelete,setDelete] = useState(false);
 
 //create a function that appends a new div based on input 
 
 function addTask(e){
-    const task = e.target.value;
-    const el = document.getElementById("tasks");
-    if(task != ""){
-        const newDiv= document.createElement("div");
-        newDiv.className="mt-1 mx-4"
-        newDiv.innerHTML=`<i class="fa-solid fa-thumbtack mx-3"></i>`+"  "+task+" "+`<i class="fa-regular fa-trash-can" id="delete"></i>`;
-        el.appendChild(newDiv);
-        const inputField = document.getElementById("input");
-        inputField.value="";
+    e.preventDefault();
+    // use .trim to avoid that multiple spaces get passed as input values
+    if(task.trim !== ''){
+        setList([...list,task]);
+        setTask('')
         setCount(prevCount => prevCount +1);
     }
 }
 
+function handleEvent(e){
+    setTask(e.target.value);
+}
 
+function deleteDiv(item){
+    document.getElementById(item).remove();
+}
 
-
-   
 
     return(
         <div className="card">
             <h2 className=" d-flex justify-content-center">Tasks</h2>
-            <input className="item" id="input" type="text" placeholder="Enter new Task" onBlur={(e)=>addTask(e)}/>
-            <div id="tasks"></div>
+            <form onSubmit={addTask}>
+                <input className="item" id="input" value={task} type="text" placeholder="Enter new Task" onChange={handleEvent}/>
+            </form>
+            <div>
+            <div id="tasks">
+                {list.map((item)=>{
+                    return(
+                    <div className="mt-1 mx-4" id={item}>
+                        <i className="fa-solid fa-thumbtack mx-3"></i>
+                        {item}
+                        <i className="fa-regular fa-trash-can" id="delete" onClick={()=>deleteDiv(item)}></i>
+                    </div>
+                    )
+                })}
+                </div>
+            </div>
             <div className="counter mt-1 mx-4" id="counter">{count}{count==1?" task left!":" tasks left!"}</div>
         </div>
     )
-}
+    }
